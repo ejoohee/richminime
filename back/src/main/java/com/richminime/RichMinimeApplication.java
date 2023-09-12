a9317fe5c04bf70a35ea20169b440d1b1fe5afa8
dbflskfl1004 <dbflskfl1004@naver.com>
2023-09-12 13:10:15 +0900

    Add project

---
+++ back/src/main/java/com/richminime/RichMinimeApplication.java
@@ -0,0 +1,13 @@
+package com.richminime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichMinimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RichMinimeApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/richminime/richminime/RichMinimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richminime.richminime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RichMinimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
