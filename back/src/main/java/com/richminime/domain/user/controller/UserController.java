9b1b0b1c476fb2683eb02c9483470737514392b5
qocksdlf123 <qocksdlf123@naver.com>
2023-09-15 10:53:09 +0900

    TEST) testtest

--- back/src/main/java/com/richminime/domain/user/controller/UserController.java
+++ back/src/main/java/com/richminime/domain/user/controller/UserController.java
@@ -16,5 +16,5 @@ public class UserController {
     public ResponseEntity<Boolean> test() {
         return ResponseEntity.ok(true);
     }
-    //test test  fdsgdagdasgsdgdasggasdggsdagdasgsagaggadsgftest test te dsaf adsfdasf fasdfgadsgasdggadsgasdg
+    //test test  fdsgdagdasgsdgdagdasgsggasdggsdagdasgsagaggadsgftest test te dsaf adsfdasf fasdfgadsgasdggadsgasdg
 }
