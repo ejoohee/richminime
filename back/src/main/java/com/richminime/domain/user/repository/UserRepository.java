d4ac27b23539a3f92aeb2569b71bee371fb5eb87
조은비 <harunz@naver.com>
2023-09-15 15:01:05 +0900

    [174] Feat(User) : 이메일 중복 검사 기능 추가 - UserRepository findByEmail 메서드 추가

--- back/src/main/java/com/richminime/domain/user/repository/UserRepository.java
+++ back/src/main/java/com/richminime/domain/user/repository/UserRepository.java
@@ -3,8 +3,11 @@ package com.richminime.domain.user.repository;
 import com.richminime.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    Optional<User> findByEmail(String email);
 
 
 }
