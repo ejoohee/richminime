97f92a11b5dc268fbf6fbf031ae602e9723865cc
이주희 <joo9837@naver.com>
2023-09-18 16:33:26 +0900

    [342]Feat(UserClothing): repo 메서드 추가

--- back/src/main/java/com/richminime/domain/clothing/dao/UserClothingRepository.java
+++ back/src/main/java/com/richminime/domain/clothing/dao/UserClothingRepository.java
@@ -1,9 +1,13 @@
 package com.richminime.domain.clothing.dao;
 
+import com.richminime.domain.clothing.constant.ClothingType;
 import com.richminime.domain.clothing.domain.UserClothing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserClothingRepository extends JpaRepository <UserClothing, Long> {
+public interface UserClothingRepository extends JpaRepository<UserClothing, Long> {
+    List<UserClothing> findAllByClothingType(ClothingType clothingType);
 }
