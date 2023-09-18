0355792a763cec0ea9f35fe25e918db479f3eb01
이주희 <joo9837@naver.com>
2023-09-18 16:36:01 +0900

    [343]Feat(UserClothing): service CRD 구현
    - addMyClothing 구현
    - deleteMyClothing 구현
    - findAllMyClothingByType 구현

--- back/src/main/java/com/richminime/domain/clothing/service/UserClothingService.java
+++ back/src/main/java/com/richminime/domain/clothing/service/UserClothingService.java
@@ -3,11 +3,14 @@ package com.richminime.domain.clothing.service;
 import com.richminime.domain.clothing.constant.ClothingType;
 import com.richminime.domain.clothing.dto.ClothingRequestDto.UserClothingUpdateRequestDto;
 import com.richminime.domain.clothing.dto.ClothingRequestDto.UserClothingCreateRequestDto;
+import com.richminime.domain.clothing.dto.ClothingResponseDto.UserClothingInfoResponseDto;
+
+import java.util.List;
 
 public interface UserClothingService {
     void addMyClothing(UserClothingCreateRequestDto userClothingCreateRequestDto);
     //내가 입고 있는 옷 정보 Character 갱신 -> 갈아입기
     void updateMyClothing(UserClothingUpdateRequestDto userClothingUpdateRequestDto);
     void deleteMyClothing(Long userClothingId);
-    void findMyClothingByType(ClothingType clothingType);
+    List<UserClothingInfoResponseDto> findAllMyClothingByType(ClothingType clothingType);
 }
--- back/src/main/java/com/richminime/domain/clothing/service/UserClothingServiceImpl.java
+++ back/src/main/java/com/richminime/domain/clothing/service/UserClothingServiceImpl.java
@@ -1,27 +1,98 @@
 package com.richminime.domain.clothing.service;
 
 import com.richminime.domain.clothing.constant.ClothingType;
+import com.richminime.domain.clothing.dao.ClothingRepository;
+import com.richminime.domain.clothing.dao.UserClothingRepository;
+import com.richminime.domain.clothing.domain.Clothing;
+import com.richminime.domain.clothing.domain.UserClothing;
 import com.richminime.domain.clothing.dto.ClothingRequestDto.UserClothingCreateRequestDto;
 import com.richminime.domain.clothing.dto.ClothingRequestDto.UserClothingUpdateRequestDto;
+import com.richminime.domain.clothing.dto.ClothingResponseDto.UserClothingInfoResponseDto;
+import com.richminime.domain.clothing.exception.ClothingNotFoundException;
+import com.richminime.domain.user.domain.User;
+import com.richminime.domain.user.exception.UserNotFoundException;
+import com.richminime.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public class UserClothingServiceImpl implements UserClothingService{
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.richminime.domain.clothing.constant.ClothingExceptionMessage.CLOTHING_NOT_FOUND;
+import static com.richminime.domain.user.exception.UserExceptionMessage.USER_NOT_FOUND;
+
+@Service
+@RequiredArgsConstructor
+public class UserClothingServiceImpl implements UserClothingService {
+
+    private final UserClothingRepository userClothingRepository;
+    private final ClothingRepository clothingRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
     @Override
     public void addMyClothing(UserClothingCreateRequestDto userClothingCreateRequestDto) {
 
+        long userId = userClothingCreateRequestDto.getUserId();
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND.getMessage()));
+
+        long clothingId = userClothingCreateRequestDto.getClothingId();
+        Clothing clothing = clothingRepository.findById(clothingId)
+                .orElseThrow(() -> new ClothingNotFoundException(CLOTHING_NOT_FOUND.getMessage()));
+
+        UserClothing userClothing = UserClothing.builder()
+                .clothing(clothing)
+                .user(user)
+                .build();
+
+        userClothingRepository.save(userClothing);
     }
 
+    @Transactional
     @Override
     public void updateMyClothing(UserClothingUpdateRequestDto userClothingUpdateRequestDto) {
-
     }
 
+    @Transactional
     @Override
     public void deleteMyClothing(Long userClothingId) {
+        UserClothing userClothing = userClothingRepository.findById(userClothingId)
+                .orElseThrow(() -> new ClothingNotFoundException(CLOTHING_NOT_FOUND.getMessage()));
 
+        User user = userClothing.getUser();
+        long newBalance = user.getBalance() + Math.round(userClothing.getClothing().getPrice() * 0.4);
+        user.updateBalance(newBalance);
+
+        //TODO bank에도 판매 log 기록
+
+        userClothingRepository.delete(userClothing);
     }
 
+    @Transactional
     @Override
-    public void findMyClothingByType(ClothingType clothingType) {
-
+    public List<UserClothingInfoResponseDto> findAllMyClothingByType(ClothingType clothingType) {
+        if (clothingType == null) {
+            List<UserClothing> userClothingList = userClothingRepository.findAll();
+            List<UserClothingInfoResponseDto> userClothingInfoResponseDtoList = new ArrayList<>();
+            for (UserClothing userClothing : userClothingList) {
+                UserClothingInfoResponseDto dto = UserClothingInfoResponseDto.builder()
+                        .userclothing(userClothing)
+                        .build();
+                userClothingInfoResponseDtoList.add(dto);
+            }
+            return userClothingInfoResponseDtoList;
+        } else {
+            List<UserClothing> userClothingListByType = userClothingRepository.findAllByClothingType(clothingType);
+            List<UserClothingInfoResponseDto> userClothingInfoResponseDtoListByType = new ArrayList<>();
+            for (UserClothing userClothing : userClothingListByType) {
+                UserClothingInfoResponseDto dto = UserClothingInfoResponseDto.builder()
+                        .userclothing(userClothing)
+                        .build();
+                userClothingInfoResponseDtoListByType.add(dto);
+            }
+            return userClothingInfoResponseDtoListByType;
+        }
     }
 }
