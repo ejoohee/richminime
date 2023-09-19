e8662646afd45714177a78af7b597eb55ceea5ac
이주희 <joo9837@naver.com>
2023-09-19 17:59:41 +0900

    [156]Feat(Clothing): Dto리팩토링
    - ClothingReqDto 구현
    - ClothingUpdateReqDto 구현
    - ClothingResDto 구현

---
+++ back/src/main/java/com/richminime/domain/clothing/dto/ClothingResDto.java
@@ -0,0 +1,28 @@
+package com.richminime.domain.clothing.dto;
+
+import com.richminime.domain.clothing.constant.ClothingType;
+import com.richminime.domain.clothing.domain.Clothing;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ClothingResDto {
+
+    private final Long clothingId;
+    private final String clothingName;
+    private final ClothingType clothingType;
+    private final String clothingImg;
+    private final String clothingInfo;
+    private final Long price;
+
+    public ClothingResDto entityToDto(Clothing clothing) {
+        return ClothingResDto.builder()
+                .clothingName(clothing.getClothingName())
+                .clothingType(clothing.getClothingType())
+                .clothingImg(clothing.getClothingImg())
+                .clothingInfo(clothing.getClothingInfo())
+                .price(clothing.getPrice())
+                .build();
+    }
+}
