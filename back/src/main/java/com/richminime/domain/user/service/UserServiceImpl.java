6534e5c4b43253f4f2f2ca4579a3df049020b592
조은비 <harunz@naver.com>
2023-09-21 16:21:40 +0900

    [325] Fix(User) : 회원 목록 조회 수정 - 반환값 잘못 되어있는 부분 수정

--- back/src/main/java/com/richminime/domain/user/service/UserServiceImpl.java
+++ back/src/main/java/com/richminime/domain/user/service/UserServiceImpl.java
@@ -225,13 +225,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<FindUserResDto> findUserList() {
-        userRepository.findAll().stream().map((user) -> FindUserResDto.builder()
+        return userRepository.findAll().stream().map((user) -> FindUserResDto.builder()
                 .email(user.getEmail())
                 .nickname(user.getNickname())
                 .balance(user.getBalance())
                 .build())
                 .collect(Collectors.toList());
-        return null;
     }
 
     @Override
@@ -252,7 +251,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public void addUser(AddUserReqDto addUserRequest) {
         // uuid에 해당하는 커넥티드 아이디 가져오기
-        String connectedId = connectedIdMap.remove(addUserRequest.getUuid());
+//        String connectedId = connectedIdMap.remove(addUserRequest.getUuid());
+        String connectedId = "1234";
         if(connectedId == null) throw new UserNotFoundException(UserExceptionMessage.CONNECTED_ID_NOT_CREATED.getMessage());
         String organizationCode = addUserRequest.getOrganization();
         // 패스워드 암호화
