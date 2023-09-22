357788f1f79a9bb67c9db39120183c1af6b39b63
이주희 <joo9837@naver.com>
2023-09-22 17:48:27 +0900

    [349]Feat(BankBook): Repo 구현

--- back/src/main/java/com/richminime/domain/bankBook/dao/BankBookRepository.java
+++ back/src/main/java/com/richminime/domain/bankBook/dao/BankBookRepository.java
@@ -1,7 +1,14 @@
 package com.richminime.domain.bankBook.dao;
 
+import com.richminime.domain.bankBook.constant.TransactionType;
 import com.richminime.domain.bankBook.domain.BankBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BankBookRepository extends JpaRepository<BankBook, Long> {
+    List<BankBook> findAllByUserId(Long userId);
+
+    List<BankBook> findAllByUserIdAndTransactionType(Long userId, TransactionType transactionType);
+
 }
