a61f88eba881e46b3f22e19c6967bf9a9fc5b0a7
MoonHongWoong <ansghddnd12@naver.com>
2023-09-22 15:29:10 +0900

    [300] Feat : 통장 페이지 생성

--- front/richminime/lib/main.dart
+++ front/richminime/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:richminime/screens/bankbook.dart';
 import 'package:richminime/screens/login.dart';
 
 void main() {
@@ -22,7 +23,7 @@ class App extends StatelessWidget {
           color: Color(0xFFEEb4a2),
         )),
       ),
-      home: const Login(),
+      home: const BankBook(),
     );
   }
 }
