b071e899f47481ed6612c91930c1c8840d931c71
MoonHongWoong <ansghddnd12@naver.com>
2023-09-24 19:35:25 +0900

    Feat : 커넥티드 아이디 발급 시 로딩 이펙트 추가

--- front/richminime/lib/screens/sign_up3.dart
+++ front/richminime/lib/screens/sign_up3.dart
@@ -45,6 +45,7 @@ class _SignUp3State extends State<SignUp3> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _animation;
   double percent = 0.5;
+  bool isLoading = false;
   @override
   void initState() {
     super.initState();
@@ -65,6 +66,9 @@ class _SignUp3State extends State<SignUp3> with SingleTickerProviderStateMixin {
   }
 
   Future<void> onNextButtonTap() async {
+    setState(() {
+      isLoading = true; // 로딩 시작
+    });
     // Navigator.push(
     //   context,
     //   MaterialPageRoute(
@@ -89,7 +93,9 @@ class _SignUp3State extends State<SignUp3> with SingleTickerProviderStateMixin {
         },
       ),
     );
-
+    setState(() {
+      isLoading = false; // 로딩 완료
+    });
     if (response.statusCode == 201) {
       if (!context.mounted) return;
       Navigator.push(
@@ -112,106 +118,115 @@ class _SignUp3State extends State<SignUp3> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // grid 같은 레이아웃을 사용하기 위해 Scaffold 위젯 사용
-      backgroundColor: Theme.of(context).colorScheme.background, // 배경색 지정
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          // Column 위젯을 사용하여 위젯을 세로로 배치
-          children: [
-            const SizedBox(height: 20),
-            LinearPercentIndicator(
-              alignment: MainAxisAlignment.center,
-              width: MediaQuery.of(context).size.width,
-              animation: false,
-              animationDuration: 1200,
-              lineHeight: 30,
-              percent: percent,
-              center: const Text('3/4'),
-              barRadius: const Radius.circular(16),
-              progressColor: Colors.red[200],
-            ),
-            const SizedBox(height: 100),
-            Text(
-              '카드사 정보를 입력해주세요',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.w900,
-                color: Theme.of(context).textTheme.bodyLarge?.color,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              alignment: Alignment.topCenter,
-              child: SizedBox(
-                width: 300,
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: cardEmailController,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                        labelText: '아이디',
-                        fillColor: Color(0xFFFFFDFD),
-                        filled: true,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    TextField(
-                      controller: cardPasswordController,
-                      obscureText: true, // 비밀번호 숨기기 옵션
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                        labelText: '비밀번호',
-                        fillColor: Color(0xFFFFFDFD),
-                        filled: true,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    TextField(
-                      controller: cardNumController,
-                      keyboardType: TextInputType.number,
-                      maxLength: 19,
-                      inputFormatters: [CardNumberInputFormatter()],
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                        labelText: '카드번호',
-                        fillColor: Color(0xFFFFFDFD),
-                        filled: true,
-                        counterText: "",
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    GestureDetector(
-                      onTap: onNextButtonTap,
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: 110,
-                        height: 50,
-                        decoration: const BoxDecoration(
-                          color: Color(0xFFFFBEBE),
-                        ),
-                        child: const Text(
-                          "다음",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                LinearPercentIndicator(
+                  alignment: MainAxisAlignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  animation: false,
+                  animationDuration: 1200,
+                  lineHeight: 30,
+                  percent: percent,
+                  center: const Text('3/4'),
+                  barRadius: const Radius.circular(16),
+                  progressColor: Colors.red[200],
+                ),
+                const SizedBox(height: 100),
+                Text(
+                  '카드사 정보를 입력해주세요',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w900,
+                    color: Theme.of(context).textTheme.bodyLarge?.color,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  alignment: Alignment.topCenter,
+                  child: SizedBox(
+                    width: 300,
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: cardEmailController,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 10),
+                            labelText: '아이디',
+                            fillColor: Color(0xFFFFFDFD),
+                            filled: true,
                           ),
                         ),
-                      ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: cardPasswordController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 10),
+                            labelText: '비밀번호',
+                            fillColor: Color(0xFFFFFDFD),
+                            filled: true,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: cardNumController,
+                          keyboardType: TextInputType.number,
+                          maxLength: 19,
+                          inputFormatters: [CardNumberInputFormatter()],
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 10),
+                            labelText: '카드번호',
+                            fillColor: Color(0xFFFFFDFD),
+                            filled: true,
+                            counterText: "",
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        GestureDetector(
+                          onTap: onNextButtonTap,
+                          child: Container(
+                            alignment: Alignment.center,
+                            width: 110,
+                            height: 50,
+                            decoration: const BoxDecoration(
+                              color: Color(0xFFFFBEBE),
+                            ),
+                            child: const Text(
+                              "다음",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
+              ],
+            ),
+          ),
+          if (isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.5),
+              child: const Center(
+                child: CircularProgressIndicator(),
               ),
             ),
-          ],
-        ),
+        ],
       ),
     );
   }
