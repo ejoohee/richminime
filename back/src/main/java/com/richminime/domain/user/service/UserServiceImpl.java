2fbd1ebf2a54060a8018258bda4c795550f56ff1
eunbypa <harunz@naver.com>
2023-09-24 18:28:00 +0900

    [333] Feat(User) : 카드 번호 RSA 암호화 복호화 기능 추가

--- back/src/main/java/com/richminime/domain/user/service/UserServiceImpl.java
+++ back/src/main/java/com/richminime/domain/user/service/UserServiceImpl.java
@@ -15,7 +15,9 @@ import com.richminime.global.common.jwt.JwtExpirationEnums;
 import com.richminime.global.exception.NotFoundException;
 import com.richminime.global.exception.TokenException;
 import com.richminime.global.util.jwt.JWTUtil;
+import com.richminime.global.util.rsa.RSAUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -53,6 +55,10 @@ public class UserServiceImpl implements UserService {
     private final LogoutAccessTokenRedisRepository logoutAccessTokenRepository;
     private final RedisTemplate<String, Object> redisTemplate;
     private final JavaMailSender javaMailSender;
+
+    @Value("${rsa.public-key}")
+    private String publicKey;
+
 
     private Map<UUID, String> connectedIdMap = new HashMap<>();
 
@@ -260,6 +266,12 @@ public class UserServiceImpl implements UserService {
         String organizationCode = addUserRequest.getOrganization();
         // 패스워드 암호화
         addUserRequest.setPassword(passwordEncoder.encode(addUserRequest.getPassword()));
+        // 카드 번호 RSA 공개키로 암호화
+        try {
+            addUserRequest.setCardNumber(RSAUtil.encryptRSA(addUserRequest.getCardNumber(), RSAUtil.publicKeyFromString(publicKey)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         userRepository.save(addUserRequest.toEntity(connectedId, organizationCode));
     }
 
