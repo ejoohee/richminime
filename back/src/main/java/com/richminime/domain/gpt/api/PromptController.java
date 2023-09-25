9139303fee8e59fa56e068c991f8f5c698a12d82
qocksdlf123 <qocksdlf123@naver.com>
2023-09-25 09:43:02 +0900

    FIX) gpt api 수정

--- back/src/main/java/com/richminime/domain/gpt/api/PromptController.java
+++ back/src/main/java/com/richminime/domain/gpt/api/PromptController.java
@@ -3,10 +3,7 @@ package com.richminime.domain.gpt.api;
 import com.richminime.domain.gpt.service.PromptService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -17,7 +14,7 @@ public class PromptController {
     private final PromptService promptService;
 
     @PostMapping
-    public Mono<String> findChatbotReply(String request){
+    public Mono<String> findChatbotReply(@RequestBody  String request){
         return promptService.findChatbotReply(request);
     }
 
