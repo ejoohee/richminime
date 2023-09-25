2a942c6740b77c3d1de5facfd8e260333792b5a2
dbflskfl1004 <dbflskfl1004@naver.com>
2023-09-25 15:59:20 +0900

    [376]Fix(Item, UserItem, Feedback): RequestHeader token 삭제

--- back/src/main/java/com/richminime/domain/feedback/api/FeedbackController.java
+++ back/src/main/java/com/richminime/domain/feedback/api/FeedbackController.java
@@ -13,32 +13,31 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/feedback")
 public class FeedbackController {
 
-    private final String ACCESS_TOKEN = "AccessToken";
     private final FeedbackService feedbackService;
 
     // 피드백 등록
     @PostMapping
-    public ResponseEntity<FeedbackResDto> addFeedback(@RequestBody FeedbackReqDto feedbackReqDto, @RequestHeader(ACCESS_TOKEN) String token) {
+    public ResponseEntity<FeedbackResDto> addFeedback(@RequestBody FeedbackReqDto feedbackReqDto) {
         return null;
     }
 
     //  피드백 수정
     @PutMapping("/{feedbackId}")
-    public ResponseEntity<FeedbackResDto> updateFeedback(@PathVariable Long feedbackId, @RequestBody FeedbackReqDto feedbackReqDto, @RequestHeader(ACCESS_TOKEN) String token) {
+    public ResponseEntity<FeedbackResDto> updateFeedback(@PathVariable Long feedbackId, @RequestBody FeedbackReqDto feedbackReqDto) {
         return null;
     }
 
 
     // 피드백 삭제
     @DeleteMapping("/{feedbackId}")
-    public ResponseEntity<MessageDto> deleteFeedback(@PathVariable Long feedbackId, @RequestHeader(ACCESS_TOKEN) String token) {
-        feedbackService.deleteFeedback(feedbackId, token);
+    public ResponseEntity<MessageDto> deleteFeedback(@PathVariable Long feedbackId) {
+        feedbackService.deleteFeedback(feedbackId);
         return ResponseEntity.ok(MessageDto.msg("DELETE SUCCUESS"));
     }
 
     // 유저별 소비패턴에 따른 1일1회 랜덤 추출
     @GetMapping
-    public ResponseEntity<FeedbackResDto> findFeedback(@RequestHeader(ACCESS_TOKEN) String token) {
+    public ResponseEntity<FeedbackResDto> findFeedback() {
         return null;
     }
 
