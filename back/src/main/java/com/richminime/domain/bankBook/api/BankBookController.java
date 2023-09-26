52bc854199e3a6b17a32d6b9d78339cf67e6ace4
dbflskfl1004 <dbflskfl1004@naver.com>
2023-09-26 14:34:26 +0900

    [376]Fix(bankbook controller): import io.swagger.v3.oas.annotations.Operation;추가

--- back/src/main/java/com/richminime/domain/bankBook/api/BankBookController.java
+++ back/src/main/java/com/richminime/domain/bankBook/api/BankBookController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
 
 import java.util.List;
 
