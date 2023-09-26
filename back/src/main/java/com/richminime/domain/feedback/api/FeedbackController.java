a39632e02328bfba765d6324556dcfe9df780a0e
dbflskfl1004 <dbflskfl1004@naver.com>
2023-09-26 14:29:05 +0900

    [372]Feat(Feedback): Controller 구현

--- back/src/main/java/com/richminime/domain/feedback/api/FeedbackController.java
+++ back/src/main/java/com/richminime/domain/feedback/api/FeedbackController.java
@@ -4,41 +4,55 @@ import com.richminime.domain.feedback.dto.FeedbackReqDto;
 import com.richminime.domain.feedback.dto.FeedbackResDto;
 import com.richminime.domain.feedback.service.FeedbackService;
 import com.richminime.global.dto.MessageDto;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/feedback")
+@RequestMapping("/feedback")
 public class FeedbackController {
 
     private final FeedbackService feedbackService;
 
-    // 피드백 등록
+    @Operation(
+            summary = "피드백 등록",
+            description = "관리자 회원만 피드백을 새로 등록할 수 있습니다."
+    )
     @PostMapping
     public ResponseEntity<FeedbackResDto> addFeedback(@RequestBody FeedbackReqDto feedbackReqDto) {
-        return null;
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(feedbackService.addFeedback(feedbackReqDto));
     }
 
-    //  피드백 수정
+    @Operation(
+            summary = "피드백 수정",
+            description = "관리자 회원만 피드백을 수정할 수 있습니다."
+    )
     @PutMapping("/{feedbackId}")
     public ResponseEntity<FeedbackResDto> updateFeedback(@PathVariable Long feedbackId, @RequestBody FeedbackReqDto feedbackReqDto) {
-        return null;
+        return ResponseEntity.ok(feedbackService.updateFeedback(feedbackId, feedbackReqDto));
     }
 
-
-    // 피드백 삭제
+    @Operation(
+            summary = "피드백 삭제",
+            description = "관리자 회원만 피드백을 삭제할 수 있습니다."
+    )
     @DeleteMapping("/{feedbackId}")
     public ResponseEntity<MessageDto> deleteFeedback(@PathVariable Long feedbackId) {
         feedbackService.deleteFeedback(feedbackId);
-        return ResponseEntity.ok(MessageDto.msg("DELETE SUCCUESS"));
+        return ResponseEntity.ok(MessageDto.msg("DELETE SUCCESS"));
     }
 
-    // 유저별 소비패턴에 따른 1일1회 랜덤 추출
+    @Operation(
+            summary = "피드백 추천",
+            description = "로그인 유저별 소비패턴에 따른 1일 1회 피드백 랜덤 추천"
+    )
     @GetMapping
     public ResponseEntity<FeedbackResDto> findFeedback() {
-        return null;
+        return ResponseEntity.ok(feedbackService.findFeedback());
     }
 
 
