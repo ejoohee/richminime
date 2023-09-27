a49b206bdde7ff8e277ec0bee532cdcde377aae3
조은비 <harunz@naver.com>
2023-09-27 10:27:46 +0900

    [363] Feat(Spending) : 매일 자정마다 전날 소비한 금액을 바탕으로 사이버머니 지급 기능 추가

--- back/src/test/java/com/richminime/richminime/RichMinimeApplicationTests.java
+++ back/src/test/java/com/richminime/richminime/RichMinimeApplicationTests.java
@@ -1,14 +1,14 @@
-package com.richminime.richminime;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RichMinimeApplicationTests {
-
-	@Test
-	void contextLoads() {
-
-	}
-
-}
+//package com.richminime.richminime;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RichMinimeApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//
+//	}
+//
+//}
