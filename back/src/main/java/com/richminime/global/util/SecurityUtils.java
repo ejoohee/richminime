31817c352572fe22b8501b1852f253045311ba37
dbflskfl1004 <dbflskfl1004@naver.com>
2023-09-27 15:56:29 +0900

    [377]Fix(Security): 테스트중

--- back/src/main/java/com/richminime/global/util/SecurityUtils.java
+++ back/src/main/java/com/richminime/global/util/SecurityUtils.java
@@ -33,43 +33,49 @@ public class SecurityUtils {
     }
 
     public String getLoggedInUserEmail() {
-        try {
-//            Authentication authentication = Objects.requireNonNull(SecurityContextHolder
-//                    .getContext()
-//                    .getAuthentication());
+//        try {
+////            Authentication authentication = Objects.requireNonNull(SecurityContextHolder
+////                    .getContext()
+////                    .getAuthentication());
+////
+////            if (authentication instanceof AnonymousAuthenticationToken) {
+////                authentication = null;
+////            }
 //
-//            if (authentication instanceof AnonymousAuthenticationToken) {
-//                authentication = null;
-//            }
-
-            getAuthentication();
-
-            //이메일정보
-            return authentication.getName();
-        } catch (NullPointerException e) {
-            throw new ForbiddenException();
-        }
+//            getAuthentication();
+//
+//            //이메일정보
+//            return authentication.getName();
+//        } catch (NullPointerException e) {
+//            throw new ForbiddenException();
+//        }
+//
+        getAuthentication();
+        return authentication.getName();
     }
 
     public Long getUserNo() {
-        try {
-//            Authentication authentication = Objects.requireNonNull(SecurityContextHolder
-//                    .getContext()
-//                    .getAuthentication());
+//        try {
+////            Authentication authentication = Objects.requireNonNull(SecurityContextHolder
+////                    .getContext()
+////                    .getAuthentication());
+////
+////            if (authentication instanceof AnonymousAuthenticationToken) {
+////                authentication = null;
+////            }
+////
+////            CustomUserDetails userPrincipal = (CustomUserDetails) authentication.getPrincipal();
 //
-//            if (authentication instanceof AnonymousAuthenticationToken) {
-//                authentication = null;
-//            }
+//            getAuthentication();
 //
-//            CustomUserDetails userPrincipal = (CustomUserDetails) authentication.getPrincipal();
-
-            getAuthentication();
-
-            // userId(번호)
-            return userPrincipal.getUserId();
-        } catch (NullPointerException e) {
-            throw new ForbiddenException();
-        }
+//            // userId(번호)
+//            return userPrincipal.getUserId();
+//        } catch (NullPointerException e) {
+//            throw new ForbiddenException();
+//        }
+        getAuthentication();
+        return userPrincipal.getUserId();
     }
+
 }
 
