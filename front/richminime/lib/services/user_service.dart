80e5650962c44c4252932257fbe06e83d3f9041e
MoonHongWoong <ansghddnd12@naver.com>
2023-09-27 14:43:43 +0900

    Feat : 메인 화면 CircularMenu 추가

--- front/richminime/lib/services/user_service.dart
+++ front/richminime/lib/services/user_service.dart
@@ -10,10 +10,52 @@ import 'package:richminime/screens/login.dart';
 import 'package:richminime/screens/sign_up.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'dart:async';
+import 'package:http_interceptor/http_interceptor.dart';
+import 'package:cookie_jar/cookie_jar.dart';
+
+const String baseUrl = Api.BASE_URL;
+const storage = FlutterSecureStorage();
+
+class HttpInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    final token = await storage.read(key: "accessToken");
+    if (token != null) {
+      data.headers["Authorization"] = "Bearer $token";
+    }
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    if (data.statusCode == 401) {
+      String? accessToken = await storage.read(key: "accessToken");
+      var cj = CookieJar();
+      List<Cookie> cookies = await cj.loadForRequest(Uri.parse(baseUrl));
+      String? refreshToken;
+      for (var cookie in cookies) {
+        if (cookie.name == 'refresh_token') {
+          refreshToken = cookie.value;
+          break;
+        }
+      }
+      final url = Uri.parse('$baseUrl/user/reissue-token');
+      final response = await http.post(
+        url,
+        headers: {
+          "Authorization": "Bearer $accessToken",
+          "Refresh-Token": "$refreshToken",
+        },
+      );
+      await const FlutterSecureStorage()
+          .write(key: "accessToken", value: response.body);
+    }
+    return data;
+  }
+}
 
 class UserService {
-  static const String baseUrl = Api.BASE_URL;
-  static const storage = FlutterSecureStorage();
+  final client = InterceptedClient.build(interceptors: [HttpInterceptor()]);
 
   Future<bool> login(String email, String password) async {
     final url = Uri.parse('$baseUrl/user/login');
@@ -27,6 +69,8 @@ class UserService {
         },
       ),
     );
+    print(response.statusCode);
+    print(response.body.toString());
     if (response.statusCode == 200) {
       final Map<String, dynamic> responseData = jsonDecode(response.body);
       final String? token = responseData['accessToken'];
