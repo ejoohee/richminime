9ff5f629a7ebfaca1bde22f4b16800fc86db96f6
Yoonoi <dbddbd39@naver.com>
2023-09-29 21:43:30 +0900

    Fix(UserItem) : Service 구현 완료

--- back/src/main/java/com/richminime/domain/item/service/UserItemService.java
+++ back/src/main/java/com/richminime/domain/item/service/UserItemService.java
@@ -1,12 +1,9 @@
 package com.richminime.domain.item.service;
 
-import com.richminime.domain.item.domain.ItemType;
-import com.richminime.domain.item.dto.ItemResDto;
-import com.richminime.domain.item.dto.ItemSearchCondition;
+import com.richminime.domain.item.constant.ItemType;
 import com.richminime.domain.item.dto.UserItemResDto;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserItemService {
     // 1. 소유한 테마 전체 조회
@@ -18,17 +15,10 @@ public interface UserItemService {
     // 소유한 테마 카테고리별 조회
     List<UserItemResDto> findAllUserItemByType(String token, ItemType itemType);
 
-    // 5-1. 소유한 테마 적용하기
-    // 5-2. 소유한 테마 벗기기
-//    UserItemResDto updateUserItem(Long userItemId);
-
     // 소유하지 않은 테마 구매하기
     UserItemResDto addUserItem(String token, Long itemId);
 
     // 6. 소유한 테마 판매하기
     void deleteUserItem(String token, Long userItemId);
-
-    // 2. 소유한 테마 조건별 조회(확장)
-//    List<UserItemResDto> findAllUserItemByCondition(ItemSearchCondition condition, String token);
 
 }
