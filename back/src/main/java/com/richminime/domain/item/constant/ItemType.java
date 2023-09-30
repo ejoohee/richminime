efbbba841b63dbe3e43efe8f6437e4d8b502664e
Yoonoi <dbddbd39@naver.com>
2023-09-30 22:05:52 +0900

    Fix(Feedback & Item): Type enum 한글로 변경

--- back/src/main/java/com/richminime/domain/item/constant/ItemType.java
+++ back/src/main/java/com/richminime/domain/item/constant/ItemType.java
@@ -5,14 +5,9 @@ import lombok.Getter;
 @Getter
 public enum ItemType {
 
-    ITEM_THEME_SET("테마"), // 가구 + 벽지 + 바닥 세트
-    ITEM_FURNITURE_SET("가구세트"), // 가구만 세트
-    ITEM_WALLPAPER_SET("벽지세트"), // 벽지 + 바닥 세트
-    
-    // 아래 3개는 나중에 확장할 예정
-    ITEM_FURNITURE("가구"),
-    ITEM_WALLPAPER("벽지"),
-    ITEM_MAT("장판");
+    벽지장판("벽지장판"),
+    가구("가구"),
+    러그("러그");
 
     private String value;
 
@@ -21,26 +16,14 @@ public enum ItemType {
     }
 
     public static ItemType getItemType(String value) {
-        if(value.contains("테마"))
-            return ITEM_THEME_SET;
-
-        else if(value.contains("가구세트"))
-            return ITEM_FURNITURE_SET;
-
-        else if(value.contains("벽지세트"))
-            return ITEM_WALLPAPER_SET;
-        
+        if(value.contains("벽지"))
+            return 벽지장판;
         else if(value.contains("가구"))
-            return ITEM_FURNITURE;
-        
-        else if(value.contains("벽지"))
-            return ITEM_WALLPAPER;
-        
-        else if(value.contains("장판"))
-            return ITEM_MAT;
-        
-        else 
-            return null; // 나중에 확장
+            return 가구;
+        else if(value.contains("러그"))
+            return 러그;
+        else
+            return null; // 확장
     }
 
 }
