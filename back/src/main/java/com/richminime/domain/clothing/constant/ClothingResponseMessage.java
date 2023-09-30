818fc548c0e3aef90719955c3722ba2c8e2930aa
joo9837 <joo9837@naver.com>
2023-09-30 22:10:48 +0900

    [414]Feat(Response): 응답 멘트 변경

--- back/src/main/java/com/richminime/domain/clothing/constant/ClothingResponseMessage.java
+++ back/src/main/java/com/richminime/domain/clothing/constant/ClothingResponseMessage.java
@@ -1,15 +1,15 @@
 package com.richminime.domain.clothing.constant;
 
 public enum ClothingResponseMessage {
-    ADD_CLOTHING("의상 등록 완료"),
-    UPDATE_CLOTHING("의상 수정 완료"),
-    DELETE_CLOTHING("의상 삭제 완료"),
-    FIND_ALL_CLOTHING("의상 목록 조회 완료"),
-    FIND_ONE_CLOTHING("의상 상세 조회 완료"),
+    ADD_CLOTHING("의상 등록 완료했습니다."),
+    UPDATE_CLOTHING("의상 수정 완료했습니다."),
+    DELETE_CLOTHING("의상 삭제 완료했습니다."),
+    FIND_ALL_CLOTHING("의상 목록 조회 완료했습니다."),
+    FIND_ONE_CLOTHING("의상 상세 조회 완료했습니다."),
 
-    ADD_MY_CLOTHING("의상 구매 완료"),
-    DELETE_MY_CLOTHING("의상 판매 완료"),
-    FIND_ALL_MY_CLOTHING("내가 보유한 의상 목록 조회");
+    ADD_MY_CLOTHING("의상 구매 완료했습니다."),
+    DELETE_MY_CLOTHING("의상 판매 완료했습니다."),
+    FIND_ALL_MY_CLOTHING("내가 보유한 의상 목록 조회 완료했습니다.");
 
 
     private final String message;
