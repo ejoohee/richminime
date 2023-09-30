81fbdc65c3f49f79c5bf51c70f2fa48b90703806
joo9837 <joo9837@naver.com>
2023-09-30 22:10:31 +0900

    [414]Feat(Exception): 예외 멘트 변경

--- back/src/main/java/com/richminime/domain/clothing/constant/ClothingExceptionMessage.java
+++ back/src/main/java/com/richminime/domain/clothing/constant/ClothingExceptionMessage.java
@@ -1,9 +1,9 @@
 package com.richminime.domain.clothing.constant;
 
 public enum ClothingExceptionMessage {
-    CLOTHING_NOT_FOUND("의상 조회 실패"),
-    CLOTHING_DUPLICATED("의상 중복"),
-    CLOTHING_COUNT_OVER("의상 수량 초과");
+    CLOTHING_NOT_FOUND("의상 조회에 실패했습니다."),
+    CLOTHING_DUPLICATED("의상이 이미 존재합니다."),
+    CLOTHING_COUNT_OVER("내 옷장이 가득 찼습니다.");
 
     private final String message;
 
