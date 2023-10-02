ae7d75afbc2b52660e459a9141d12832a0f6e14e
heeya1morestep <heeya1morestep@gmail.com>
2023-10-03 01:31:13 +0900

    Feat: 외부, item rest api통신 연결

--- front/richminime/lib/screens/interior_store.dart
+++ front/richminime/lib/screens/interior_store.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animated_button/flutter_animated_button.dart';
+import 'package:richminime/models/interior_theme_model.dart';
+import 'package:richminime/services/interior_service.dart';
 
 class InteriorStore extends StatefulWidget {
   const InteriorStore({super.key});
@@ -8,7 +11,47 @@ class InteriorStore extends StatefulWidget {
 }
 
 class _InteriorStoreState extends State<InteriorStore> {
+  final List<String> categories = ["벽지장판", "러그", "가구"];
+  int selectedCategoryIndex = 0; // 선택된 카테고리 인덱스
+
   int selectedIndex = 0;
+  String miniRoomImgLink = '';
+  final InteriorService interiorService = InteriorService();
+  List<InteriorThemeModel> itemList = [];
+  List<InteriorThemeModel> sortedItemList = [];
+
+// 전체 불러오기
+  @override
+  void initState() {
+    super.initState();
+    loadItemData();
+    sortedItemList = itemList
+        .where((item) => item.itemType == categories[selectedCategoryIndex])
+        .toList();
+  }
+
+  Future<void> loadItemData() async {
+    try {
+      final loadedItemList = await interiorService.getAllItems();
+      setState(() {
+        itemList = loadedItemList;
+      });
+    } catch (e) {
+      // 에러 처리
+      print("Error loading item data: $e");
+    }
+  }
+
+  // 우선 탭 = 미리보기
+  int tappedIndex = 3000000;
+  themeTap(int index) {
+    setState(() {});
+    tappedIndex = index;
+  }
+
+  // 테마 사기
+  applyTheme() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,71 +63,147 @@ class _InteriorStoreState extends State<InteriorStore> {
             children: [
               Flexible(
                 fit: FlexFit.tight,
-                flex: 5,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Flexible(
-                      fit: FlexFit.tight,
-                      flex: 1,
-                      child: Container(
-                        clipBehavior: Clip.hardEdge,
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 7, vertical: 10),
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).cardColor,
-                          borderRadius: BorderRadius.circular(7),
+                flex: 6,
+                child: Container(
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.black.withOpacity(0.2),
+                  ),
+                  child: tappedIndex == 3000000
+                      ? const Center(child: Text('골라봐~~'))
+                      : Row(
+                          children: [
+                            Flexible(
+                              fit: FlexFit.tight,
+                              flex: 1,
+                              child: Stack(children: [
+                                Container(
+                                  clipBehavior: Clip.hardEdge,
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 7, vertical: 10),
+                                  decoration: BoxDecoration(
+                                    color: Colors.black.withOpacity(0.3),
+                                    borderRadius: BorderRadius.circular(7),
+                                  ),
+                                  child: const Center(
+                                    child: Text(
+                                      '전',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Center(
+                                    child: Image.network(
+                                  sortedItemList[tappedIndex].itemImg ?? '',
+                                  fit: BoxFit.cover,
+                                ))
+                              ]),
+                            ),
+                            Flexible(
+                              fit: FlexFit.tight,
+                              flex: 1,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                  Text(
+                                    '<${sortedItemList[tappedIndex].itemName}>',
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  Expanded(
+                                    child: ShaderMask(
+                                      shaderCallback: (Rect bounds) {
+                                        return LinearGradient(
+                                          //아래 속성들을 조절하여 원하는 값을 얻을 수 있다.
+                                          begin: Alignment.center,
+                                          end: Alignment.topCenter,
+                                          colors: [
+                                            Colors.white,
+                                            Colors.white.withOpacity(0.02)
+                                          ],
+                                          stops: const [0.8, 1],
+                                          tileMode: TileMode.mirror,
+                                        ).createShader(bounds);
+                                      },
+                                      child: SingleChildScrollView(
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 15, vertical: 8),
+                                          child: Text(
+                                            '<${sortedItemList[tappedIndex].itemInfo}>',
+                                            overflow: TextOverflow
+                                                .clip, // Overflow 발생 시 글 내용을 자르지 않고 표시
+                                            style: const TextStyle(
+                                              fontSize: 17,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  Material(
+                                    elevation: 3,
+                                    color: Theme.of(context).cardColor,
+                                    shadowColor: Colors.black54,
+                                    borderRadius: BorderRadius.circular(5),
+                                    child: InkWell(
+                                        splashColor: Colors.white54,
+                                        onTap: () {},
+                                        borderRadius: BorderRadius.circular(5),
+                                        child: const Padding(
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: 5, horizontal: 12),
+                                          child: Text(
+                                            "구매하기",
+                                            style: TextStyle(
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        )),
+                                  ),
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
                         ),
-                        child: const Center(
-                            child: Text(
-                          '전',
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                        )),
-                      ),
-                    ),
-                    Flexible(
-                      fit: FlexFit.tight,
-                      flex: 1,
-                      child: Container(
-                        clipBehavior: Clip.hardEdge,
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 7, vertical: 10),
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).cardColor,
-                          borderRadius: BorderRadius.circular(7),
-                        ),
-                        child: const Center(
-                            child: Text(
-                          '후',
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                        )),
-                      ),
-                    ),
-                  ],
                 ),
               ),
 
               // 테마+아이템이 들어갈 시 주어질 버튼
-              // Padding(
-              //   padding: const EdgeInsets.symmetric(horizontal: 10),
-              //   child: SingleChildScrollView(
-              //     scrollDirection: Axis.horizontal, // 수평 스크롤 가능
-              //     child: Row(
-              //       children: makeButtons(categories),
-              //     ),
-              //   ),
-              // ),
               Flexible(
                 fit: FlexFit.tight,
-                flex: 4,
+                flex: 1,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal, // 수평 스크롤 가능
+                  child: Row(
+                    children: makeButtons(categories),
+                  ),
+                ),
+              ),
+              Flexible(
+                fit: FlexFit.tight,
+                flex: 5,
                 child: ShaderMask(
                   shaderCallback: (Rect bounds) {
                     return LinearGradient(
-                      //아래 속성들을 조절하여 원하는 값을 얻을 수 있다.
                       begin: Alignment.topCenter,
                       end: Alignment.topRight,
                       colors: [Colors.white, Colors.white.withOpacity(0.02)],
@@ -95,27 +214,23 @@ class _InteriorStoreState extends State<InteriorStore> {
                   child: Padding(
                     padding:
                         const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                    child: ListView.separated(
+                    child: ListView.builder(
                       padding: const EdgeInsets.symmetric(
                           horizontal: 15, vertical: 5),
                       scrollDirection: Axis.horizontal,
-                      itemCount: 30,
+                      itemCount: sortedItemList.length,
                       itemBuilder: (context, index) {
                         return GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              selectedIndex = index;
-                            });
-                          },
+                          onTap: () => themeTap(index),
                           child: Container(
                             clipBehavior: Clip.hardEdge,
-                            margin: const EdgeInsets.all(3),
-                            //임시값
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 3, horizontal: 10),
                             width: 150,
                             decoration: BoxDecoration(
                               color: Theme.of(context).cardColor,
                               borderRadius: BorderRadius.circular(5),
-                              border: selectedIndex == index
+                              border: tappedIndex == index
                                   ? Border.all(width: 3, color: Colors.white38)
                                   : null,
                               boxShadow: [
@@ -127,15 +242,15 @@ class _InteriorStoreState extends State<InteriorStore> {
                               ],
                             ),
                             child: Center(
-                              child: Text(
-                                'Themes $index',
+                              child: Image.network(
+                                sortedItemList[index].itemImg ??
+                                    '', // 이미지 URL을 itemList에서 가져오기
+                                fit: BoxFit.cover, // 이미지가 컨테이너에 맞게 확대/축소
                               ),
                             ),
                           ),
                         );
                       },
-                      separatorBuilder: (context, index) =>
-                          const SizedBox(width: 15),
                     ),
                   ),
                 ),
@@ -148,23 +263,47 @@ class _InteriorStoreState extends State<InteriorStore> {
   }
 
   // 테마+아이템이 들어갈 시 주어질 버튼
-  // List<Widget> makeButtons(List<String> categories) {
-  //   List<Widget> buttons = [];
+  List<Widget> makeButtons(List<String> categories) {
+    List<Widget> buttons = [];
 
-  //   for (String category in categories) {
-  //     buttons.add(
-  //       Container(
-  //         margin: const EdgeInsets.all(8), // 버튼 간격 조정
-  //         child: ElevatedButton(
-  //           onPressed: () {
-  //             // 버튼을 눌렀을 때 실행될 코드
-  //           },
-  //           child: Text(category), // 각 버튼에 카테고리 텍스트 표시
-  //         ),
-  //       ),
-  //     );
-  //   }
+    for (int index = 0; index < categories.length; index++) {
+      buttons.add(
+        Container(
+          margin: const EdgeInsets.all(8), // 버튼 간격 조정
+          child: AnimatedButton(
+            textAlignment: Alignment.center,
+            height: 35,
+            width: 70,
+            text: categories[index],
+            isReverse: false,
+            isSelected: selectedCategoryIndex == index ? true : false,
+            selectedBackgroundColor: Theme.of(context).cardColor,
+            selectedTextColor: Colors.black,
+            transitionType: TransitionType.LEFT_TO_RIGHT,
+            textStyle: const TextStyle(
+                color: Colors.black54,
+                fontFamily: 'StarDustS',
+                fontWeight: FontWeight.w700,
+                fontSize: 18),
+            backgroundColor: const Color(0xFFFFBEBE).withOpacity(0.2),
+            borderColor: Colors.white38,
+            borderRadius: 5,
+            borderWidth: 2,
+            onPress: () {
+              setState(() {
+                selectedCategoryIndex = index; // 카테고리 선택 시 인덱스 업데이트
+                final selectedCategory = categories[selectedCategoryIndex];
+                sortedItemList = itemList
+                    .where((item) => item.itemType == selectedCategory)
+                    .toList();
+              });
+            },
+            // 각 버튼에 카테고리 텍스트 표시
+          ),
+        ),
+      );
+    }
 
-  //   return buttons;
-  // }
+    return buttons;
+  }
 }
--- front/richminime/lib/services/interior_service.dart
+++ front/richminime/lib/services/interior_service.dart
@@ -1,33 +1,82 @@
 import 'dart:convert';
 
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:richminime/constants/api.dart';
 import 'package:richminime/models/interior_theme_model.dart';
 
-class InteriorService {
-  static const String baseUrl = Api.BASE_URL;
+const storage = FlutterSecureStorage();
+const String baseUrl = Api.BASE_URL;
 
+class InteriorService {
   // 전체 테마 불러오기(테마 상점)
-  Future<List<InteriorThemeModel>> getAllThemes() async {
+  Future<dynamic> getAllItems() async {
     List<InteriorThemeModel> interiorInstances = [];
 
     final url = Uri.parse('$baseUrl/item');
+    final token = await storage.read(key: "accessToken");
+    final headers = {
+      'Authorization': 'Bearer $token', // accessToken을 헤더에 추가
+    };
 
-    final response = await http.get(url);
+    try {
+      print('아이템받으러 간다잇');
+      final response = await http.get(
+        url,
+        headers: headers,
+      );
+      print('바디 : ${response.body}');
+      print('아이템받기 : ${response.statusCode}');
 
-    if (response.statusCode == 200) {
-      final List<dynamic> themes = jsonDecode(response.body);
-      for (var theme in themes) {
-        interiorInstances.add(InteriorThemeModel.fromJson(theme));
+      if (response.statusCode == 200) {
+        final List<dynamic> themes = jsonDecode(response.body);
+        for (var theme in themes) {
+          interiorInstances.add(InteriorThemeModel.fromJson(theme));
+        }
+
+        return interiorInstances;
       }
-
-      return interiorInstances;
+    } catch (e) {
+      print('에러는 말이죵 : $e');
     }
-    throw Error();
+    return null;
+  }
+
+  // 카테고리별 아이템 불러오기
+  Future<dynamic> getAllItemsByCategory(String itemType) async {
+    List<InteriorThemeModel> interiorInstances = [];
+
+    final url = Uri.parse('$baseUrl/item?itemType=$itemType');
+    final token = await storage.read(key: "accessToken");
+    final headers = {
+      'Authorization': 'Bearer $token', // accessToken을 헤더에 추가
+    };
+
+    try {
+      print('아이템받으러 간다잇');
+      final response = await http.get(
+        url,
+        headers: headers,
+      );
+      print('바디 : ${response.body}');
+      print(response.statusCode);
+
+      if (response.statusCode == 200) {
+        final List<dynamic> themes = jsonDecode(response.body);
+        for (var theme in themes) {
+          interiorInstances.add(InteriorThemeModel.fromJson(theme));
+        }
+
+        return interiorInstances;
+      }
+    } catch (e) {
+      print('에러는 말이죵 : $e');
+    }
+    return null;
   }
 
   // 소유한 전체 테마 불러오기-my
-  Future<List<InteriorThemeModel>> getMyAllThemes() async {
+  Future<List<InteriorThemeModel>> getMyAllItems() async {
     List<InteriorThemeModel> myInteriorInstances = [];
 
     final url = Uri.parse('$baseUrl/item/my');
